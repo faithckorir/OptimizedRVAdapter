f28baf2200773f105c5b95da4f09ce0db680ea77
Faith Chemkemoi <fchepkemoi@hq.ekenya.co.ke>
2022-06-30 10:45:13 +0300

    manipulating itemview

--- app/src/main/java/com/finixstore/optimizingrv/MainActivity.kt
+++ app/src/main/java/com/finixstore/optimizingrv/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.finixstore.optimizingrv.databinding.ActivityMainBinding
+import com.finixstore.optimizingrv.databinding.ItemListBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rv: RecyclerView
     private lateinit var fruitAdapter: BaseRecyclerViewAdapter<String>
     private lateinit var binding: ActivityMainBinding
+    private lateinit var itemBinding: ItemListBinding
     private val dummyData = listOf(
         "Apple",
         "Banana",
@@ -28,21 +30,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        itemBinding = ItemListBinding.inflate(layoutInflater)
+
         fruitAdapter = BaseRecyclerViewAdapter(
             R.layout.item_list,
             false
         ) { position, data, view ->
             // init view on recycler view adapter
-            val tvNumber =
-                view.findViewById<AppCompatTextView>(R.id.tv_number)
-            val tvFruit =
-                view.findViewById<AppCompatTextView>(R.id.tv_fruit)
+            val tvNumber = view.findViewById<AppCompatTextView>(R.id.tv_number)
+            val tvFruit = view.findViewById<AppCompatTextView>(R.id.tv_fruit)
+
 
             // set data to view
             tvNumber.text = "Fruit #${position + 1}"
             tvFruit.text = data
         }
         rv = findViewById(R.id.rv)
+        //binding.rv.apply {  }
 
         rv.layoutManager =
             LinearLayoutManager(this, RecyclerView.VERTICAL, false)
